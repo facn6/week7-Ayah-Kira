c502f587dba94cbc46a24aa9aec7ee33472179ac
ayah <ayah_kh5@hotmail.com>
2019-08-20 17:15:15 +0300

    app

--- public/app.js
+++ public/app.js
@@ -1,25 +1,25 @@
-"use strict";
-
-const bcrypt = require("bcryptjs");
-
-const hashPassword = (password) => {
-  bcrypt.genSalt(10, (err, salt) => {
-    if (err) {
-  return new error('there is an error');
-    } else {
-      bcrypt.hash(password, salt, (err,hash)=>{
-        if(err){
-           return new error('there is an error');
-         }
-        else{
-        return hash;
-      }
-      });
-    }
-  });
-};
-
-
-module.exports = {
-  hashPassword
-};
+// "use strict";
+//
+// const bcrypt = require("bcryptjs");
+//
+// const hashPassword = (password) => {
+//   bcrypt.genSalt(10, (err, salt) => {
+//     if (err) {
+//   return new error('there is an error');
+//     } else {
+//       bcrypt.hash(password, salt, (err,hash)=>{
+//         if(err){
+//            return new error('there is an error');
+//          }
+//         else{
+//         return hash;
+//       }
+//       });
+//     }
+//   });
+// };
+//
+//
+// module.exports = {
+//   hashPassword
+// };
